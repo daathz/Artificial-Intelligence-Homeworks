f583dd0b0a75795c472b23dd1287ae182b3cbe88
Viktor Novák <novk.viktor@gmail.com>
2018-10-04 20:36:33 +0200

    A full algorithm, it works with example data

--- Labirintus/src/Main.java
+++ Labirintus/src/Main.java
@@ -2,10 +2,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static int[][] labyrinth;
+    static int[][] maze;
     static  int numberOfItems;
-    static int positionX = 0;
-    static int positionY = 0;
+    static int actualRow = 0;
+    static int actualColumn = 0;
+    static int numberOfRows;
+    static int numberOfColumns;
+
 
     public static void read_labyrinth() {
         Scanner scanner = new Scanner(System.in);
@@ -21,24 +24,52 @@ public class Main {
             }
         }
         String[] stemp = temp.get(0).split(" ");
-        int numberOfRows = temp.size() - 1;
-        int numberOfColumns = stemp.length;
+        numberOfRows = temp.size() - 1;
+        numberOfColumns = stemp.length;
         numberOfItems = Integer.parseInt(temp.get(temp.size() - 1));
-        labyrinth = new int[numberOfRows][numberOfColumns];
+        maze = new int[numberOfRows][numberOfColumns];
         for (int i = 0; i < temp.size() - 1; ++i) {
             stemp = temp.get(i).split(" ");
             for (int j = 0; j < stemp.length; ++j) {
-                labyrinth[i][j] = Integer.parseInt(stemp[j]);
+                maze[i][j] = Integer.parseInt(stemp[j]);
             }
         }
-        System.out.println("asd");
     }
 
-    public static void findTheItems() {
-        
+    public static void oneStep() {
+        int value = maze[actualRow][actualColumn];
+        boolean isItem = (value & 16) != 0;
+        boolean cantGoNorth = (value & 1) != 0;
+        boolean cantGoEast = (value & 2) != 0;
+        boolean cantGoSouth = (value & 4) != 0;
+        boolean cantGoWest = (value & 8) != 0;
+
+        if (isItem) {
+            System.out.println("felvesz");
+        }
+
+        if (!cantGoEast && actualColumn + 1 < numberOfColumns) {
+            actualColumn++;
+            System.out.println(actualRow + " " + actualColumn);
+        }
+        else if (!cantGoSouth && actualRow + 1 < numberOfRows) {
+            actualRow++;
+            System.out.println(actualRow + " " + actualColumn);
+        }
+        else if (!cantGoWest && actualColumn > 0) {
+            actualColumn--;
+            System.out.println(actualRow + " " + actualColumn);
+        }
+        else if (!cantGoNorth && actualRow > 0) {
+            actualRow--;
+            System.out.println(actualRow + " " + actualColumn);
+        }
     }
 
     public static void main(String[] args) {
         read_labyrinth();
+        while((actualRow != numberOfRows - 1) || (actualColumn != numberOfColumns - 1)) {
+            oneStep();
+        }
     }
 }
