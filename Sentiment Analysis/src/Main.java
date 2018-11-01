0e05d43c05edce58518730295bf9b9b4aae1e0f1
Viktor Novák <novk.viktor@gmail.com>
2018-11-01 17:54:34 +0100

    Semi-complete bayes

--- Sentiment Analysis/src/Main.java
+++ Sentiment Analysis/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -6,7 +8,6 @@ public class Main {
     static ArrayList<int[]> inputData = new ArrayList<>();
     static int[] inputLabels = new int[80000];
     static ArrayList<int[]> targetData = new ArrayList<>();
-    static int[] targetLabels = new int[20000];
 
     public static void readInput() {
         Scanner scanner = new Scanner(System.in);
@@ -48,6 +49,83 @@ public class Main {
         }
     }
 
+    public static HashMap<Integer, ArrayList<Integer>> makeInitialVocabulary() {
+        HashMap<Integer, ArrayList<Integer>> vocabulary = new HashMap<>();
+        for (int i = 0; i < 80000; ++i) {
+            try {
+                for (int j = 0; j < inputData.get(i).length; ++j) {
+                    if (!vocabulary.containsKey(inputData.get(i)[j])) {
+                        ArrayList<Integer> values = new ArrayList<>();
+                        values.add(inputLabels[i]);
+                        vocabulary.put(inputData.get(i)[j], values);
+                    } else {
+                        ArrayList<Integer> values = vocabulary.get(inputData.get(i)[j]);
+                        values.add(inputLabels[i]);
+                        vocabulary.replace(inputData.get(i)[j], values);
+                    }
+                }
+            } catch (NullPointerException e) {
+            }
+        }
+
+        return vocabulary;
+    }
+
+    public static HashMap<Integer, Double> makeZeroVocabulary(HashMap<Integer, ArrayList<Integer>> initial) {
+        HashMap<Integer, Double> vocabulary = new HashMap<>();
+        for (int key : initial.keySet()) {
+            ArrayList<Integer> list = initial.get(key);
+            int counter = 0;
+            for (int labels : list) {
+                if (labels == 0) {
+                    counter++;
+                }
+            }
+            double result = counter / (double) list.size();
+            vocabulary.put(key, result);
+        }
+
+        return vocabulary;
+    }
+
+    public static HashMap<Integer, Double> makeOneVocabulary(HashMap<Integer, ArrayList<Integer>> initial) {
+        HashMap<Integer, Double> vocabulary = new HashMap<>();
+        for (int key : initial.keySet()) {
+            ArrayList<Integer> list = initial.get(key);
+            int counter = 1;
+            for (int labels : list) {
+                if (labels == 0) {
+                    counter++;
+                }
+            }
+            double result = counter / (double) list.size();
+            vocabulary.put(key, result);
+        }
+
+        return vocabulary;
+    }
+
+    public static void printResult(HashMap<Integer, Double> zeros, HashMap<Integer, Double> ones) {
+        for (int i = 0; i < 20000; ++i) {
+            double zeroProbability = 0.5;
+            double oneProbability = 0.5;
+            int[] document = targetData.get(i);
+            try {
+                for (int j = 0; j < document.length; ++j) {
+                    zeroProbability *= zeros.get(document[j]);
+                    oneProbability *= ones.get(document[j]);
+                }
+                if (zeroProbability > oneProbability) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(1);
+                }
+            } catch (NullPointerException e) {
+                System.out.println(0);
+            }
+        }
+    }
+
     public static void generateRandomOutput() {
         for (int i = 0; i < 20000; ++i) {
             System.out.println(Math.round(Math.random()));
@@ -56,6 +134,9 @@ public class Main {
 
     public static void main(String[] args) {
         readInput();
-        generateRandomOutput();
+        HashMap<Integer, ArrayList<Integer>> initialVocabulary = makeInitialVocabulary();
+        HashMap<Integer, Double> zeroVocabulary = makeZeroVocabulary(initialVocabulary);
+        HashMap<Integer, Double> oneVocabulary = makeOneVocabulary(initialVocabulary);
+        printResult(zeroVocabulary, oneVocabulary);
     }
 }
