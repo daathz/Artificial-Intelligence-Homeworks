692ae10e0c9a0299f718576574628a252b1497f4
Viktor Novák <novk.viktor@gmail.com>
2018-11-01 13:40:13 +0100

    Fix reading input data

--- Sentiment Analysis/generate_data_to_sentiment_analysis.py
+++ Sentiment Analysis/generate_data_to_sentiment_analysis.py
@@ -2,8 +2,8 @@ from random import randint
 
 with open('random_data_for_homework.txt', 'w') as f:
     # inputData
-    for i in range(8000):
-        x = randint(5, 15)
+    for i in range(80000):
+        x = randint(1, 15)
         for j in range(x):
             if j < x - 1:
                 print(randint(0, 200), end='\t', file=f)
@@ -11,12 +11,12 @@ with open('random_data_for_homework.txt', 'w') as f:
                 print(randint(0, 200), end='\n', file=f)
 
     # inputLabels
-    for i in range(2000):
+    for i in range(80000):
         print(randint(0, 1), file=f)
 
     # targetData
-    for i in range(8000):
-        x = randint(5, 15)
+    for i in range(20000):
+        x = randint(1, 15)
         for j in range(x):
             if j < x - 1:
                 print(randint(0, 200), end='\t', file=f)
--- Sentiment Analysis/src/Main.java
+++ Sentiment Analysis/src/Main.java
@@ -1,36 +1,61 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     static ArrayList<int[]> inputData = new ArrayList<>();
-    static int[] inputLabels = new int[8000];
+    static int[] inputLabels = new int[80000];
     static ArrayList<int[]> targetData = new ArrayList<>();
-    static int[] targetLabels = new int[2000];
+    static int[] targetLabels = new int[20000];
 
     public static void readInput() {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 8000; ++i) {
+
+        for (int i = 0; i < 80000; ++i) {
             String temp = scanner.nextLine();
-            String[] tempArray = temp.split("\t");
-            int[] intArray = Arrays.asList(tempArray).stream().mapToInt(Integer::parseInt).toArray();
-            inputData.add(intArray);
+            if (temp.equals("")) {
+                inputData.add(null);
+            } else {
+                String[] tempArray = temp.split("\\t");
+                int[] intArray = new int[tempArray.length];
+                for (int j = 0; j < intArray.length; ++j) {
+                    try {
+                        intArray[j] = Integer.parseInt(tempArray[j]);
+                    } catch (NumberFormatException e) {
+                        System.out.println(i);
+                    }
+                }
+                inputData.add(intArray);
+            }
         }
 
-        for (int i = 0; i < 8000; ++i) {
+        for (int i = 0; i < 80000; ++i) {
             inputLabels[i] = Integer.parseInt(scanner.nextLine());
         }
 
-        for (int i = 0; i < 2000; ++i) {
+        for (int i = 0; i < 20000; ++i) {
             String temp = scanner.nextLine();
-            String[] tempArray = temp.split("\t");
-            int[] intArray = Arrays.asList(tempArray).stream().mapToInt(Integer::parseInt).toArray();
-            inputData.add(intArray);
+            if (temp.equals("")) {
+                targetData.add(null);
+            } else {
+                String[] tempArray = temp.split("\\t");
+                int[] intArray = new int[tempArray.length];
+                for (int j = 0; j < intArray.length; ++j) {
+                    intArray[j] = Integer.parseInt(tempArray[j]);
+                }
+                targetData.add(intArray);
+            }
+        }
+    }
+
+    public static void generateRandomOutput() {
+        for (int i = 0; i < 20000; ++i) {
+            System.out.println(Math.round(Math.random()));
         }
     }
 
     public static void main(String[] args) {
         readInput();
+        generateRandomOutput();
     }
 }
